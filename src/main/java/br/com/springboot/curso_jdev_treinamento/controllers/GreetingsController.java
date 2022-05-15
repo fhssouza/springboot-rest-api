712300b669fcf95203fcfe7c9510f3323e5ffa93
Fábio Souza <62041832+fhssouza@users.noreply.github.com>
2022-05-15 19:45:50 -0300

    criando o end-point de listar todos os usuarios

--- src/main/java/br/com/springboot/curso_jdev_treinamento/controllers/GreetingsController.java
+++ src/main/java/br/com/springboot/curso_jdev_treinamento/controllers/GreetingsController.java
@@ -1,11 +1,20 @@
 package br.com.springboot.curso_jdev_treinamento.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.springboot.curso_jdev_treinamento.model.Usuario;
+import br.com.springboot.curso_jdev_treinamento.repository.UsuarioRepository;
 
 /**
  *
@@ -14,6 +23,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingsController {
 	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
 		
     /**
      *
@@ -26,5 +37,12 @@ public class GreetingsController {
         return "Curso Spring Boot API " + name + "!";
     }
     
-    
+	@GetMapping(value = "listatodos")
+	@ResponseBody
+    public ResponseEntity<List<Usuario>> listaUsuario(){
+    	
+		List<Usuario> usuarios = usuarioRepository.findAll();
+		
+		return new ResponseEntity<List<Usuario>>(usuarios, HttpStatus.OK);
+    }
 }
