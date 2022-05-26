66fc097e2d67e10fddb8209590b942eaafd1b2f2
fhs_s <fhs_s@MAQ01DESK>
2022-05-26 19:49:17 -0300

    Criando uma tela MODAL para pesquisar e editar com END-POINT

--- src/main/java/br/com/springboot/curso_jdev_treinamento/controllers/GreetingsController.java
+++ src/main/java/br/com/springboot/curso_jdev_treinamento/controllers/GreetingsController.java
@@ -78,6 +78,16 @@ public class GreetingsController {
 		return new ResponseEntity<Usuario>(user, HttpStatus.OK);
 	}
 	
+	@GetMapping(value = "buscarPorNome")
+	@ResponseBody
+	public ResponseEntity<List<Usuario>> buscarPorNome(@RequestParam(name = "name") String name){
+		
+		List<Usuario> usuario = usuarioRepository.buscarPorNome(name.trim().toUpperCase());
+		
+		return new ResponseEntity<List<Usuario>>(usuario, HttpStatus.OK);
+	}
+	
+	
 	@PutMapping(value = "atualizar")
 	@ResponseBody
 	public ResponseEntity<?> atualizar(@RequestBody Usuario usuario){
